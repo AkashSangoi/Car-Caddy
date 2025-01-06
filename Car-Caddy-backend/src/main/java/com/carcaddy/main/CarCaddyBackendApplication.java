d29c6cdf66b3c1de5c15c01b6a2d7dca8ee08a81
AkashSangoi <akashverma8331@gmail.com>
2025-01-06 22:54:17 +0530

    Initial commit with project files

---
+++ Car-Caddy-backend/src/main/java/com/carcaddy/main/CarCaddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carcaddy.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCaddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCaddyBackendApplication.class, args);
+	}
+
+}
